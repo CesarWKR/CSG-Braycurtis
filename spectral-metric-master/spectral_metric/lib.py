3c3476b0b552ea40c64e01d75a7ca1b0bcb37973
CesarWKR <cesarwkr1@gmail.com>
2024-07-09 22:12:28 -0500

    New changes

--- spectral-metric-master/spectral_metric/lib.py
+++ spectral-metric-master/spectral_metric/lib.py
@@ -8,7 +8,8 @@ import scipy as sp
 import scipy.stats
 from sklearn.metrics import pairwise_distances
 
-from spectral_metric.types import Array, SimilarityArrays
+#from spectral_metric.my_types import Array, SimilarityArrays
+from my_types import Array, SimilarityArrays
 
 log = logging.getLogger(__name__)
 pjoin = os.path.join
