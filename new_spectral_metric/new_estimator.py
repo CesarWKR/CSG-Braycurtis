bc20ff9ce3017ff8363566c3da4f102b6256af38
CesarWKR <cesarwkr1@gmail.com>
2024-07-21 18:46:19 -0500

    New metric and fixxes in the code

---
+++ new_spectral_metric/new_estimator.py
@@ -0,0 +1,85 @@
+
+import logging
+from itertools import product
+import numpy as np
+import scipy
+import scipy.spatial
+from numpy.linalg import LinAlgError
+from scipy.sparse.csgraph import laplacian
+import pickle
+from new_lib import find_samples, compute_expectation_with_monte_carlo
+
+log = logging.getLogger(__name__)
+
+class CumulativeGradientEstimator(object):
+    def __init__(self, M_sample=250, k_nearest=10, distance="euclidean"):
+        self.M_sample = M_sample  # Tama�o m�ximo de muestra por clase
+        self.k_nearest = k_nearest
+        self.distance = distance
+        self.P = {}  # Initialize P attribute
+        self.M = None  # Initialize M attribute
+
+    def fit(self, data, target):
+        np.random.seed(None)
+        data_x = data.copy()
+        self.n_class = np.max(target) - min(0, np.min(target)) + 1
+
+        # Pasar self.M_sample a find_samples
+        class_samples, self.class_indices = find_samples(
+            data_x, target, self.n_class, M=self.M_sample
+        )
+
+        self.compute(data_x, target, class_samples)
+        return self
+
+    def compute(self, data, target, class_samples):
+        self.S, self.similarity_arrays = compute_expectation_with_monte_carlo(
+            data,
+            target,
+            class_samples,
+            class_indices=self.class_indices,
+            n_class=self.n_class,
+            k_nearest=self.k_nearest,
+            distance=self.distance,
+        )
+
+        self.W = np.eye(self.n_class)
+        for i, j in product(range(self.n_class), range(self.n_class)):
+            self.W[i, j] = 1 - scipy.spatial.distance.braycurtis(self.S[i], self.S[j])
+
+        self.difference = 1 - self.W
+
+        self.L_mat, dd = laplacian(self.W, False, True)
+        try:
+            self.evals, self.evecs = np.linalg.eigh(self.L_mat)
+            self.csg = self._csg_from_evals(self.evals)
+        except LinAlgError as e:
+            log.warning(f"{str(e)}; assigning `evals,evecs,csg` to NaN")
+            self.evals = np.ones([self.n_class]) * np.nan
+            self.evecs = np.ones([self.n_class, self.n_class]) * np.nan
+            self.csg = np.nan
+
+        # Compute the M matrix for samples
+        self.M = np.zeros((data.shape[0], data.shape[0]))
+        for i in range(data.shape[0]):
+            for j in range(data.shape[0]):
+                self.M[i, j] = 1 - scipy.spatial.distance.braycurtis(data[i], data[j])
+
+        for i, j in product(range(self.n_class), range(self.n_class)):
+            class_i_indices = self.class_indices[i]
+            class_j_indices = self.class_indices[j]
+            similarity_matrix = np.zeros((len(class_i_indices), len(class_j_indices)))
+            for idx_i, i_idx in enumerate(class_i_indices):
+                for idx_j, j_idx in enumerate(class_j_indices):
+                    similarity_matrix[idx_i, idx_j] = 1 - scipy.spatial.distance.braycurtis(data[i_idx], data[j_idx])
+            self.P[(i, j)] = similarity_matrix   # Compute the matriz to compare the pair classes
+
+    def _csg_from_evals(self, evals: np.ndarray) -> float:
+        grads = evals[1:] - evals[:-1]
+        ratios = grads / (np.array([list(reversed(range(1, grads.shape[-1] + 1)))]) + 1)
+        csg: float = np.maximum.accumulate(ratios, -1).sum(1)
+        return csg
+
+if __name__ == "__main__":
+    pass
+    
---
+++ new_spectral_metric/new_lib.py
@@ -0,0 +1,84 @@
+import logging
+import os
+from collections import defaultdict
+from typing import Tuple, Dict
+
+import numpy as np
+import scipy as sp
+import scipy.stats
+from sklearn.metrics import pairwise_distances
+
+from my_types import Array, SimilarityArrays
+
+log = logging.getLogger(__name__)
+pjoin = os.path.join
+
+def compute_expectation_with_monte_carlo(
+    data: Array,
+    target: Array,
+    class_samples: Dict[int, Array],
+    class_indices: Dict[int, Array],
+    n_class: int,
+    k_nearest=10,
+    distance: str = "euclidean",
+) -> Tuple[Array, Dict[int, Dict[int, SimilarityArrays]]]:
+    def get_volume(dt):
+        dst = (np.abs(dt[1:] - dt[0]).max(0) * 2).prod()
+        res = max(1e-4, dst)
+        return res
+
+    similarity_arrays: Dict[int, Dict[int, SimilarityArrays]] = defaultdict(dict)
+    expectation = np.zeros([n_class, n_class])  # S-matrix
+
+    similarities = lambda k: np.array(pairwise_distances(class_samples[k], data, metric=distance))
+
+    for class_ix in class_samples:
+        all_similarities = similarities(class_ix)  # Distance arrays for all class samples
+        all_indices = class_indices[class_ix]  # Indices for all class samples
+        for m, sample_ix in enumerate(all_indices):
+            indices_k = all_similarities[m].argsort()[: k_nearest + 1]  # kNN indices (incl self)
+            target_k = np.array(target)[indices_k[1:]]  # kNN class labels (self is dropped)
+            probability = np.array(
+                [(target_k == nghbr_class).sum() / k_nearest for nghbr_class in range(n_class)]
+            )
+            probability_norm = probability / get_volume(data[indices_k])  # Parzen-window normalized
+            similarity_arrays[class_ix][sample_ix] = SimilarityArrays(
+                sample_probability=probability, sample_probability_norm=probability_norm
+            )
+            expectation[class_ix] += probability_norm
+
+        expectation[class_ix] /= expectation[class_ix].sum()
+
+    expectation[np.logical_not(np.isfinite(expectation))] = 0
+
+    log.info("----------------Diagonal--------------------")
+    log.info(np.round(np.diagonal(expectation), 4))
+    return expectation, similarity_arrays
+
+def find_samples(
+    data: np.ndarray, target: np.ndarray, n_class: int, M=100, seed=None
+) -> Tuple[Dict[int, Array], Dict[int, Array]]:
+    rng = np.random.RandomState(seed)
+    class_samples = {}
+    class_indices = {}
+    indices = np.arange(len(data))
+    for k in np.unique(target):
+        indices_in_cls = rng.permutation(indices[target == k])
+        to_take = min(M if M > 1 else int(M * len(indices_in_cls)), len(indices_in_cls))
+        class_samples[k] = data[indices_in_cls[:to_take]]
+        class_indices[k] = indices_in_cls[:to_take]
+    return class_samples, class_indices
+
+def get_cummax(eigens: np.ndarray) -> Tuple[float, float]:
+    def mean_confidence_interval(data, confidence=0.95):
+        a = 1.0 * np.array(data)
+        n = len(a)
+        m, se = np.mean(a), sp.stats.sem(a)
+        h = se * sp.stats.t._ppf((1 + confidence) / 2.0, n - 1)
+        return m, m - h, m + h
+
+    grads = eigens[:, 1:] - eigens[:, :-1]
+    ratios = grads / (np.array([list(reversed(range(1, grads.shape[-1] + 1)))]) + 1)
+    cumsums = np.maximum.accumulate(ratios, -1).sum(1)
+    mu, lb, ub = mean_confidence_interval(cumsums)
+    return mu, ub - mu
